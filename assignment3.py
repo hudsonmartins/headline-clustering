a5c4e952eaa07fdbb44e71367d9446af37f3df86
Hudson <hudsonbr95@gmail.com>
2018-05-04 20:01:58 -0300

    Splitting the dataset in years

--- assignment3.py
+++ assignment3.py
@@ -33,39 +33,27 @@ def create_sentences_csv():
 			writer = csv.writer(csvfile, delimiter = ',', quoting=csv.QUOTE_NONE)
 			writer.writerow([row[0], sentence])
 
-def create_years_csv():
+def split_years_csv():
+	dataset = np.genfromtxt('dataset/sentences.csv', delimiter=',', dtype = None)
 
 	data = []
-	dataset = np.genfromtxt('dataset/sentences.csv', delimiter=',', dtype = None)
 	row_count = 0
+	year = 2003
 	for row in dataset:
+		fn = 'dataset/sentences_'+str(year)+'.csv'
 		if row_count == 0:
 			with open(fn, 'a') as csvfile:
 				writer = csv.writer(csvfile, delimiter = ',', quoting=csv.QUOTE_NONE)
 				writer.writerow(row)
 			row_count +=1
 			continue
-		tokens = nltk.word_tokenize(row[1])
-		tagged = nltk.pos_tag(tokens) #Getting tags
-		sentence = ''
 		
-		#Removing tags from the sentences
-		count_tag = 0
-		for tags in tagged:
-			if tags[1] not in remove:	
-				if count_tag != len(tagged) - 1:
-					sentence += tags[0]+' '
-				else:
-					sentence += tags[0]
-			count_tag += 1 			
-		row_count +=1
-				
-		with open(fn, 'a') as csvfile:
-			writer = csv.writer(csvfile, delimiter = ',', quoting=csv.QUOTE_NONE)
-			writer.writerow([row[0], sentence])
-
-
-
+		if str(year) == row[0][0:4]:			
+			with open(fn, 'a') as csvfile:
+				writer = csv.writer(csvfile, delimiter = ',', quoting=csv.QUOTE_NONE)
+				writer.writerow(row)
+		else:
+			year += 1
 
 def get_headlines():
 
@@ -87,4 +75,5 @@ def get_headlines():
 
 data = get_headlines()
 #create_sentences_csv()
+#split_years_csv()
 extract_features.get_features_2gram(data)
