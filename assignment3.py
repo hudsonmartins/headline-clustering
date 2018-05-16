0a22fef96bd7390a88df478bd0413d612d906c8a
Hudson <hudsonbr95@gmail.com>
2018-05-16 18:19:30 -0300

    Improving elbow rule

--- assignment3.py
+++ assignment3.py
@@ -1,11 +1,15 @@
 import numpy as np
-import csv, glob, os, extract_features, random
+import csv, glob, os, extract_features, random, nltk
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 from nltk.stem import PorterStemmer
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize
+from sklearn.metrics import silhouette_samples, silhouette_score
+from nltk.cluster import KMeansClusterer
+
+import matplotlib.cm as cm
 
 def get_headlines(year=None, subset=None):
 
@@ -16,18 +20,18 @@ def get_headlines(year=None, subset=None):
 	headlines = []
 	dataset = np.genfromtxt('dataset/news_headlines.csv', delimiter=',', dtype = None)
 	row_count = 0
-	if subset != None:
-		num_dados = int(len(dataset)*subset)
-		print "Getting a random subset with ", num_dados, " data"
-		for i in range(num_dados):
-			index = random.randrange(1, len(dataset))
-			sentence = dataset[index]
-			headlines.append(sentence)
-	else:
-		headlines = dataset
 		
 	if year == None:
 		print "Getting headlines from all years"
+		if subset != None:
+			num_dados = int(len(dataset)*subset)
+			print "Getting a random subset with ", num_dados, " data"
+			for i in range(num_dados):
+				index = random.randrange(1, len(dataset))
+				sentence = dataset[index]
+				headlines.append(sentence)
+		else:
+			headlines = dataset
 			
 		for row in headlines:
 			if row_count == 0:
@@ -38,12 +42,11 @@ def get_headlines(year=None, subset=None):
 			row_count +=1	
 	else:
 		print "Getting headlines from ", year
-			
+		headlines=dataset	
 		for row in headlines:
 			if row_count == 0:
 				row_count +=1
 				continue
-
 			if str(year) == row[0][0:4]:
 				sentence = get_root_sentence(row[1])
 				data.append(sentence)
@@ -58,8 +61,9 @@ def get_root_sentence(sentence):
 	for word in words:
 		if len(word) < 3:
 			continue
+		if word.isdigit():
+			continue
 		root_sentence += ps.stem(word) + ' '
-		
 	return root_sentence
 
 def clusterization(data, features, k):
@@ -71,8 +75,14 @@ def clusterization(data, features, k):
 	#print "Centers: ", kmeans.cluster_centers_
 
 	pred = kmeans.predict(features)
-	result_dict = dict()
+	
+	print ("Score:", kmeans.score(features))
+	#silhouette = silhouette_score(features, kmeans.labels_, metric='euclidean', sample_size = 10000)
+	 
+	#print ("Silhouette_score: ")
+	#print (silhouette)
 
+	result_dict = dict()
 	i = 0
 	for result in pred:
 		if(result in result_dict):  #save in the dictionary
@@ -82,15 +92,15 @@ def clusterization(data, features, k):
 		i += 1
 
 	
-	
 	for i in range(len(result_dict)):
 		print "Group ", i, " lenght ", len(result_dict[i])
 		count = 0
-		top_words = extract_features.get_top_n_words(20, result_dict[i])
+		top_words = extract_features.get_top_n_words(30, result_dict[i])
 		print('Cluster '+str(i) + ' : ' + ', '.join(word for word in top_words))
-
-		"""
-		for sentence in result_dict[i]:
+		
+		top_bigrams = extract_features.get_top_grams(30, result_dict[i], '2word')
+		print('Cluster '+str(i) + ' : ' + ', '.join(word for word in top_bigrams))
+		"""for sentence in result_dict[i]:
 			print sentence	
 			count += 1
 			if(count > 20):
@@ -112,31 +122,34 @@ def tsne_visulatization(matrix, n_topics):
 def elbow_rule(data, features, max_k=10):
 	k = 2
 	cost = []
-
 	while k < max_k:
 		print "Clustering with k = ", k
 		kmeans = clusterization(data, features, k)
 		cost.append(kmeans.inertia_/features.shape[0])
 		print "Final cost = ", kmeans.inertia_/features.shape[0]
 		k+=1
-	#x = np.arange(2, max_k, 1)
-	#plt.plot(x, cost)
-	
-	#plt.show()	
+	x = np.arange(2, 20, 1.0)
+	plt.plot(x, cost)
+	plt.ylabel("Cost")
+	plt.xlabel("Number of Clusters")
+	plt.show()	
 	return cost		
 	
-	
-
-
-data = get_headlines()
+data = get_headlines(year=2009)
 print "Number of headlines: ", len(data)
 features = extract_features.get_features_4char_gram(data)
-clusterization(data, features,k=8)
+clusterization(data, features, k = 5)
+#elbow_rule(data, features, max_k=20)
 """
+sub = 0.1
 for i in range(5):
+	data = get_headlines(subset = sub)
+	print "Number of headlines: ", len(data)
+	features = extract_features.get_features_4char_gram(data)
 	cost = elbow_rule(data, features, max_k=20)
 	x = np.arange(2, 20, 1.0)
 	plt.plot(x, cost)
+	sub += 0.05
 plt.ylabel("Cost")
 plt.xlabel("Number of Clusters")
 plt.show()
--- extract_features.py
+++ extract_features.py
@@ -1,28 +1,20 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import PCA
-from sklearn.decomposition import TruncatedSVD
+#from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD as PCA
 import numpy as np
 
 def get_features_2gram(data):
 	"""
 		Creates a vector of normalized (tf-idf) features from a 2-gram bag of words
 	"""
-	vectorizer = TfidfVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (2,2), max_features = 10000) 
+	vectorizer = TfidfVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (2,2), max_features = 1000) 
 	features = vectorizer.fit_transform(data)
-	print vectorizer.get_feature_names()
+	#print vectorizer.get_feature_names()
+	pca = PCA()
+	features = pca.fit_transform(features)
+
 	return features
-	"""
-	steps = int(len(data)*0.001)
-	for i in range(0, len(data), steps):
-		print "From ", i, " to ", i+steps
-		feat = features[i:i+steps]
-		print feat.shape
-		feat =	feat.toarray()
-		pca = PCA()	
-		components = pca.fit_transform(feat)	
-		print "reduced: ", components.shape
-	"""																																																																																																																																																																																																																				
 
 	
 
@@ -31,38 +23,81 @@ def get_features_3gram(data):
 		Creates a vector of normalized (tf-idf) features from a 3-gram bag of words
 	"""
 	feat = []
-	vectorizer = TfidfVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (3,3), max_features = 10000) 
+	vectorizer = TfidfVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (3,3), max_features = 1000) 
 	feat = vectorizer.fit_transform(data)
 	return feat
 	#print vectorizer.get_feature_names()
 	#for v in feat:
 	#	print v
+
+def get_features_2n3gram(data):
+	"""
+		Creates a vector of normalized (tf-idf) features from a 2 and 3-gram bag of words
+	"""
+	feat = []
+	vectorizer = TfidfVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (2,3), max_features = 5000) 
+	feat = vectorizer.fit_transform(data)
+	pca = PCA()
+	feat = pca.fit_transform(feat)
+	#print vectorizer.get_feature_names()
+	return feat
 
 def get_features_4char_gram(data):
 	"""
 		Creates a vector of normalized (tf-idf) features from a char-4-gram bag of words
 	"""
 	feat = []
-	vectorizer = TfidfVectorizer(analyzer = 'char_wb', stop_words = 'english', ngram_range = (4,4), max_features = 1000) 
+	vectorizer = TfidfVectorizer(analyzer = 'char_wb', stop_words = 'english', ngram_range = (4,4), max_features = 5000) 
 	feat = vectorizer.fit_transform(data)
+	pca = PCA()
+	feat = pca.fit_transform(feat)
 	return feat
 	#print vectorizer.get_feature_names()
 	#for v in feat:
 	#	print v
 
 def get_top_n_words(top, sentences):
+
 	vectorizer = CountVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (1,1), max_features = top) 
 	feat = vectorizer.fit_transform(sentences)
 	
 	vectorized_total = np.sum(feat, axis=0)
 	word_indices = np.flip(np.argsort(vectorized_total)[0,:], 1)
-	word_values = np.flip(np.sort(vectorized_total)[0,:],1)
+	#word_values = np.flip(np.sort(vectorized_total)[0,:],1)
 
 	word_vectors = np.zeros((top, feat.shape[1]))
+	
 	for i in range(top):
-		word_vectors[i,word_indices[0,i]] = 1
-
+		if(i < word_indices.shape[1]):
+			word_vectors[i, word_indices[0,i]] = 1
+		
 	words = [word[0].encode('ascii').decode('utf-8') for word in vectorizer.inverse_transform(word_vectors)]
 
 	return words
 
+def get_top_grams(top, sentences, gram = '2word'):
+
+	if gram == '2word':
+		vectorizer = CountVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (2,2), max_features = top) 
+	elif gram == '3word':
+		vectorizer = CountVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (3,3), max_features = top) 
+	elif gram == '2n3word':
+		vectorizer = CountVectorizer(analyzer = 'word', stop_words = 'english', ngram_range = (2,3), max_features = top) 
+	elif gram == '4char':
+		vectorizer = CountVectorizer(analyzer = 'char_wb', stop_words = 'english', ngram_range = (4,4), max_features = top) 
+	
+	feat = vectorizer.fit_transform(sentences)
+	
+	vectorized_total = np.sum(feat, axis=0)
+	word_indices = np.flip(np.argsort(vectorized_total)[0,:], 1)
+	#word_values = np.flip(np.sort(vectorized_total)[0,:],1)
+
+	word_vectors = np.zeros((top, feat.shape[1]))
+	
+	for i in range(top):
+		if(i < word_indices.shape[1]):
+			word_vectors[i, word_indices[0,i]] = 1
+		
+	words = [word[0].encode('ascii').decode('utf-8') for word in vectorizer.inverse_transform(word_vectors)]
+
+	return words
